121f644f2c9597e56b5e232c7fee13dec6dd3382
Mayis-Mazin <mayis.mazin@gmail.com>
2021-03-05 12:56:30 +0200

    String interpolation

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -4,5 +4,12 @@ import { Component } from '@angular/core';
     templateUrl :'./card.component.html'
 })
 export class CardComponent{
-    
+    Weight:number=10;
+    height:number=160.3
+    heartBeat:number=105
+    cardStatus: string='offline'
+
+    getCardStatus(){
+        return this.cardStatus;
+    }
 }
